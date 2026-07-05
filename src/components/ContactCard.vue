03e9496a5f005f68e9a21b150c2e4132ab761d9a
Gia Bao <baodc24v7k503@vlvh.ctu.edu.vn>
2026-07-06 01:40:14 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,32 @@
+<script>
+    export default {
+        props: {
+           contact: {
+                type: Object,
+                required: true
+            }, 
+        },
+    };
+</script>
+
+<template>
+    <div>   
+        <div class="p-1">
+            <strong>Tên: {{ contact.name }}</strong> 
+        </div>
+        <div class="p-1">
+            <strong>Email: {{ contact.email }}</strong> 
+        </div>
+        <div class="p-1">
+            <strong>Địa chỉ: {{ contact.address }}</strong> 
+        </div>
+        <div class="p-1">
+            <strong>Điện thoại: {{ contact.phone }}</strong> 
+        </div>
+        <div class="p-1">
+            <strong>Liên hệ yêu thích: &nbsp </strong>
+            <i v-if="contact.isFavorite" class="fas fa-check"></i>
+            <i v-else class="fas fa-times"></i>
+        </div>
+    </div>
+</template>
